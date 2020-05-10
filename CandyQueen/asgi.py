3b18e63f08af2d0a383d7991813a98e3b9a7d270
Heidars19 <heidar.sigurjons@gmail.com>
2020-05-10 00:19:02 +0000

    dynamic links/apps

--- CandyQueen/asgi.py
+++ CandyQueen/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CandyQueen project.
+ASGI config for candyqueen project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CandyQueen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'candyqueen.settings')
 
 application = get_asgi_application()
